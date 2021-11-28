6e6169cd076fc75ae019706e58627d5dc3c5b420
Bert Van den Broeck <bertvdbroeck@hotmail.com>
2021-11-28 01:13:54 +0100

    Zoekfunctie kan nu meerdere films weergeven

--- web/js/index.js
+++ web/js/index.js
@@ -1,14 +1,14 @@
 function api() {
 	var apikey = "a1610a3d"
 	var query = document.getElementById("query").value;
-	fetch('http://www.omdbapi.com/?t=' + query + '&apikey=' + apikey)
+	fetch('http://www.omdbapi.com/?s=' + query + '&apikey=' + apikey)
 	.then((response) => response.json())
 	.then((json) => display(json));
 }
 function display(json) {
-	document.getElementById("Title").innerHTML = json.Title
-	document.getElementById("Title").href = "./film/?i=" + json.imdbID
-	document.getElementById("Year").innerHTML = json.Year
-	document.getElementById("Poster").src = json.Poster
-	document.getElementById("Poster").style.display = "block";
+	for (let i = 0; i < json.Search.length; i++) {
+		const element = document.getElementById("films");
+		var film = (`<section id="${json.Search[i].imdbID}"><h1><a href="./film/?i=${json.Search[i].imdbID}">${json.Search[i].Title}</a></h1><p>${json.Search[i].Year}</p><img src="${json.Search[i].Poster}" width="200" alt="Poster"></img></section>`);
+		element.insertAdjacentHTML('beforeend', film);
+	}
 }
