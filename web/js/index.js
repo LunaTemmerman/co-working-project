cb04061ab41de993a8177bf981c3b4a53114560c
Bert Van den Broeck <bertvdbroeck@hotmail.com>
2021-12-08 00:13:24 +0100

    Populaire films nu op de home pagina

--- web/js/index.js
+++ web/js/index.js
@@ -3,12 +3,18 @@ function api() {
 	var query = document.getElementById("query").value.replace(/\s/g, '+');
 	fetch('https://api.themoviedb.org/3/search/movie?api_key=' + apikey + '&query=' + query)
 	.then((response) => response.json())
-	.then((json) => display(json));
+	.then((json) => display(json,"films"));
 }
-function display(json) {
+function display(json, elementId) {
 	for (let i = 0; i < json.results.length; i++) {
-		const element = document.getElementById("films");
-		var film = (`<section id="${json.results[i].id}"><h1><a href="./film/?id=${json.results[i].id}">${json.results[i].title}</a></h1><p>${json.results[i].release_date}</p><img src="https://image.tmdb.org/t/p/w200${json.results[i].poster_path}" alt="Poster"></img></section>`);
+		const element = document.getElementById(elementId);
+		var film = (`<section id="${json.results[i].id}"><h1><a href="./film/?id=${json.results[i].id}">${json.results[i].title}</a></h1><p>${json.results[i].release_date.slice(0, 4)}</p><img src="https://image.tmdb.org/t/p/w200${json.results[i].poster_path}" alt="Poster"></img></section>`);
 		element.insertAdjacentHTML('beforeend', film);
 	}
+}
+function populair() {
+	var apikey = "000cfc8a44435ac1017a805bb5b2bbac"
+	fetch('https://api.themoviedb.org/3/movie/popular?api_key=' + apikey)
+	.then((response) => response.json())
+	.then((json) => display(json, "populair"));
 }
