52d7f962b7508d681b65b00d84b6f3c63f680793
LunaTemmerman <luna.temmerman@student.odisee.be>
2022-01-19 15:50:21 +0100

    subheaders

--- web/footer.php
+++ web/footer.php
@@ -1,5 +1,5 @@
 <?php
 ?>
-<footer  class="row fixed-bottom text-center p-4 bg-light text-black">
+<footer  class="row border-bottom text-center p-4 bg-light text-black">
     <p class="col">Co-Working Project - Luna, Leander, Robber & Bert  © 2021-2022 Copyright</p>
 </footer>
--- web/zoek/headersub.php
+++ web/zoek/headersub.php
@@ -1,20 +1,16 @@
-<?php
-session_start();
-?>
-
 <header>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid">
-            <a class="navbar-brand" href="../">
-                <img width="40" src="../img/logo.png" alt="logo">
-                Film Reviews</a>
+            <a class="navbar-brand" href="./">
+                <img width="40" src="../img/logo.png" class="popcorn_logo" alt="logo">
+                Film reviews</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="../">Home</a>
+                        <a class="nav-link" aria-current="page" href="./">Home</a>
                     </li>
                     <?php
                     if (isset($_SESSION["id"])) {
@@ -31,9 +27,10 @@ session_start();
                 </ul>
                 <form class="d-flex" action="javascript:searchBox('home')" method="post">
                     <input class="form-control me-2 bg-white" type="search" placeholder="Zoek naar een film" aria-label="Search" id="query">
-                    <button class="btn btn-outline-bordeaux" type="submit">🔎︎</button>
+                    <button class="btn btn-outline-light" type="submit">🔎︎</button>
                 </form>
             </div>
         </div>
     </nav>
 </header>
+
